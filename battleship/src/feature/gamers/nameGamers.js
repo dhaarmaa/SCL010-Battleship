f90f32646ddbfe67c801faca78bf8b24169b834b
adrievelyn <adrievelyn@gmail.com>
2019-12-02 20:05:09 -0300

    creando la segunda vista del juego con el nombre de los Jugadores

---
+++ battleship/src/feature/gamers/nameGamers.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+class nameGamers extends React.Component {
+	render(){
+		return (
+			<h1>Hola jugador soy Estoiko el vasto jefe de la isla Berk.
+Escoge tu nombre de vikingo para iniciar.</h1>
+		)
+	}
+}
+
+export default nameGamers;
--- battleship/src/index.js
+++ battleship/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import nameGamers from './feature/gamers/nameGamers';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const element = <h1>Hola jugador soy Estoiko el vasto jefe de la isla Berk.
+Escoge tu nombre de vikingo para iniciar.</h1>
+
+const container = document.getElementById('app');
+
+
+ReactDOM.render(element, container);
