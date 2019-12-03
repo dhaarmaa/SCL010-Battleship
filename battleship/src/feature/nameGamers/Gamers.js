f59088709866561f75f29345f50e8ac0ecc15428
adrievelyn <adrievelyn@gmail.com>
2019-12-03 12:33:20 -0300

    agregando carpeta styles

--- battleship/src/feature/nameGamers/Gamers.js
+++ battleship/src/feature/nameGamers/Gamers.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import '../../feature/nameGamers/Gamers.ccs';
+import '../../feature/styles/Gamers.ccs';
 import EstoicoImg from '../../img/Estoico.png';
 
 class Gamers extends React.Component {
 	render(){
-		return <div className='general'>
+		return (
+			<div className='general'>
 			<div className='imgGamers'>
 				<img src={EstoicoImg} alt='Estoico'/>
 			</div>
@@ -14,15 +15,17 @@ class Gamers extends React.Component {
 				<h1>Hola jugador soy Estoiko el vasto jefe de la isla Berk.
 Escoge tu nombre de vikingo para iniciar.</h1>
 			</div>
-			<div>
-				
+			<label> 
+   				<input type="to add name" name="nameGamers" />
+  			</label>
+
 			<div>
 				<button className='button'>Siguiente</button>
 			<div>
 			</div>
 		</div>
-	</div>;
-		
+	</div>
+		)
 	}
 }
 
